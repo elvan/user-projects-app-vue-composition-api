ee9a6fafab865c0d31492365836f0ffdc6eba3c5
Elvan Hidayat <elvanhd@gmail.com>
2023-03-14 03:18:16 +0700

    create a search hook

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,13 +1,11 @@
 <template>
   <base-container v-if="user">
     <h2>{{ user.fullName }}: Projects</h2>
-
     <base-search
       v-if="hasProjects"
       @search="updateSearch"
       :search-term="enteredSearchTerm"
     ></base-search>
-
     <ul v-if="hasProjects">
       <project-item
         v-for="prj in availableProjects"
@@ -15,18 +13,17 @@
         :title="prj.title"
       ></project-item>
     </ul>
-
     <h3 v-else>No projects found.</h3>
   </base-container>
-
   <base-container v-else>
     <h3>No user selected.</h3>
   </base-container>
 </template>
 
 <script>
-import { computed, ref, toRefs, watch } from 'vue';
+import { computed, toRefs, watch } from 'vue';
 
+import useSearch from '../../hooks/search.js';
 import ProjectItem from './ProjectItem.vue';
 
 export default {
@@ -35,43 +32,28 @@ export default {
   },
   props: ['user'],
   setup(props) {
-    const enteredSearchTerm = ref('');
-    const activeSearchTerm = ref('');
-
-    const availableProjects = computed(function () {
-      if (activeSearchTerm.value) {
-        return props.user.projects.filter((prj) =>
-          prj.title.includes(activeSearchTerm.value)
-        );
-      }
-      return props.user.projects;
-    });
-
-    const hasProjects = computed(function () {
-      return props.user.projects && availableProjects.value.length > 0;
-    });
-
-    watch(enteredSearchTerm, function (newValue) {
-      setTimeout(() => {
-        if (newValue === enteredSearchTerm.value) {
-          activeSearchTerm.value = newValue;
-        }
-      }, 300);
-    });
-
     const { user } = toRefs(props);
 
-    watch(user, function () {
-      enteredSearchTerm.value = '';
+    const projects = computed(function () {
+      return user.value ? user.value.projects : [];
     });
 
-    function updateSearch(val) {
-      enteredSearchTerm.value = val;
-    }
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(
+      projects,
+      'title'
+    );
+
+    const hasProjects = computed(function () {
+      return user.value.projects && availableItems.value.length > 0;
+    });
+
+    watch(user, function () {
+      updateSearch('');
+    });
 
     return {
       enteredSearchTerm,
-      availableProjects,
+      availableProjects: availableItems,
       hasProjects,
       updateSearch,
     };
