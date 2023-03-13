73bf1e478683bcd1a305f29fc61f13cfeb280d63
Elvan Hidayat <elvanhd@gmail.com>
2023-03-13 15:45:43 +0700

    project setup

---
+++ src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 'evan',
+    fullName: 'Elvan Hidayat',
+    projects: [
+      { id: 'evan_p1', title: 'Learn the Vue course' },
+      { id: 'evan_p2', title: 'Code more projects' },
+      { id: 'evan_p3', title: 'Keep projects updated' },
+    ],
+  },
+  {
+    id: 'aaron',
+    fullName: 'Aaron Ferguson',
+    projects: [
+      { id: 'aaron_p1', title: 'Create more projects' },
+      { id: 'aaron_p2', title: 'Dive into data science topics' },
+      { id: 'aaron_p3', title: 'Various things' },
+    ],
+  },
+  {
+    id: 'cindy',
+    fullName: 'Cindy Jones',
+    projects: [
+      { id: 'cindy_p1', title: 'Create agenda for next months' },
+      { id: 'cindy_p2', title: 'Explore new topics' },
+    ],
+  },
+  {
+    id: 'billy',
+    fullName: 'Billy Sanderson',
+    projects: [],
+  },
+];
